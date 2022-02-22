a464667d596f331913e027edc670d37357131b01
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-02-22 23:21:12 +0200

    базовые елементы

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,13 @@
+const startRef = document.querySelector('[data-start]');
+// console.log(startRef)
+const stopRef = document.querySelector('[data-stop]');
+// console.log(stopRef)
 
+startRef.addEventListener('click', startRandomBg);
+stopRef.addEventListener('click', stopRandomBg);
+
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
