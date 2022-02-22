c9ce63857b960d32efc834456bff0aed16d6da80
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-02-22 23:53:00 +0200

    рандомный цвет фона

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,10 +2,22 @@ const startRef = document.querySelector('[data-start]');
 // console.log(startRef)
 const stopRef = document.querySelector('[data-stop]');
 // console.log(stopRef)
+let randomColor = null;
 
 startRef.addEventListener('click', startRandomBg);
 stopRef.addEventListener('click', stopRandomBg);
 
+function startRandomBg() {
+    randomColor = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+        console.log('hello')
+    }, 1000);
+}
+
+function stopRandomBg() {
+    clearInterval(randomColor);
+    console.log('stop')
+}
 
 
 function getRandomHexColor() {
