cf4ca031be11ccd63b52ae00298bf6e652073eaa
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-04-01 21:12:37 +0300

    дата имена

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,12 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
+const input = document.querySelector('#datetime-picker');
+const btnStart = document.querySelector('[data-start]');
+const dataDays = document.querySelector('[data-days]');
+const dataHours = document.querySelector('[data-hours]');
+const dataMinutes = document.querySelector('[data-minutes]');
+const dataSeconds = document.querySelector('[data-seconds]');
 
 const options = {
   enableTime: true,
