eabd053dd8c7d9f622b321f5be8bb6557d4024dc
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-04-02 22:42:07 +0300

    копирует вызов функции

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -10,10 +10,19 @@ function submitForm(element) {
   const formAmount = parseInt(element.currentTarget.amount.value);
 
   for (let i = 1; i <= formAmount; i += 1) {
+    callStatusPromise(i, formDelay);
     formDelay += formStep;
   }
-
 }
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+  
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
