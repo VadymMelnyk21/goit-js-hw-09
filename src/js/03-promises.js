cfa09b4cc4442c1ef26ffb55a627c1113c4db54a
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-04-02 22:36:34 +0300

    сабмит формы

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,20 @@
+const formRef = document.querySelector('.form');
+
+formRef.addEventListener('submit', submitForm);
+
+function submitForm(element) {
+  element.preventDefault();
+
+  let formDelay = parseInt(element.currentTarget.delay.value);
+  const formStep = parseInt(element.currentTarget.step.value);
+  const formAmount = parseInt(element.currentTarget.amount.value);
+
+  for (let i = 1; i <= formAmount; i += 1) {
+    formDelay += formStep;
+  }
+
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
